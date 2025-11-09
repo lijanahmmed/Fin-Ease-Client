b1cdecf5bde3a55f7f141f85109b3a8140fcf3bc
lijanahmmed <lijanahmmed@gmail.com>
2025-11-10 03:33:19 +0600

    added navbar logout functionalities

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,32 @@
-import React from "react";
-import { Link, NavLink } from "react-router";
+import React, { use } from "react";
+import { Link, NavLink, useNavigate } from "react-router";
+import { AuthContext } from "../Context/AuthContext";
+import { toast } from "react-toastify";
 
 const Navbar = () => {
+  const { user, loading, logOutFunc } = use(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    logOutFunc()
+      .then(() => {
+        toast.info("Your account has been logged out");
+        navigate('/');
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
+
   const links = (
     <>
       <li>
         <NavLink
           to="/"
           className={({ isActive }) =>
-            isActive ? "text-purple-900 font-bold border-b-2" : "text-purple-500"
+            isActive
+              ? "text-purple-900 font-bold border-b-2"
+              : "text-purple-500"
           }
         >
           Home
@@ -18,7 +36,9 @@ const Navbar = () => {
         <NavLink
           to="/add-transaction"
           className={({ isActive }) =>
-            isActive ? "text-purple-900 font-bold border-b-2" : "text-purple-500"
+            isActive
+              ? "text-purple-900 font-bold border-b-2"
+              : "text-purple-500"
           }
         >
           Add Transaction
@@ -29,7 +49,9 @@ const Navbar = () => {
         <NavLink
           to="/my-transactions"
           className={({ isActive }) =>
-            isActive ? "text-purple-900 font-bold border-b-2" : "text-purple-500"
+            isActive
+              ? "text-purple-900 font-bold border-b-2"
+              : "text-purple-500"
           }
         >
           My Transactions
@@ -39,7 +61,9 @@ const Navbar = () => {
         <NavLink
           to="/reports"
           className={({ isActive }) =>
-            isActive ? "text-purple-900 font-bold border-b-2" : "text-purple-500"
+            isActive
+              ? "text-purple-900 font-bold border-b-2"
+              : "text-purple-500"
           }
         >
           Reports
@@ -77,30 +101,64 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="flex items-center">
-            <img className="w-12 rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT815NGXKz9w59G2GFzQbv18u8oXc0NxkLuLw&s" alt="" />
-            <Link to="/" className="btn btn-ghost text-purple-900 text-xl">
-          Fin Ease
-        </Link>
+          <img
+            className="w-12 rounded-full"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT815NGXKz9w59G2GFzQbv18u8oXc0NxkLuLw&s"
+            alt=""
+          />
+          <Link to="/" className="btn btn-ghost text-purple-900 text-xl">
+            Fin Ease
+          </Link>
         </div>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{links}</ul>
       </div>
       <div className="navbar-end">
-        <div className="flex gap-2">
-          <Link
-            className="btn bg-gradient-to-r from-purple-800 to-purple-500 text-white"
-            to="/auth/login"
-          >
-            Login
-          </Link>
-          <Link
-            className="btn bg-gradient-to-r from-purple-800 to-purple-500 text-white"
-            to="/auth/register"
-          >
-            Register
-          </Link>
-        </div>
+        {loading ? (
+          <span className="loading loading-spinner loading-xl"></span>
+        ) : user ? (
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="m-1 cursor-pointer">
+              <img
+                className="w-10 h-10 object-cover rounded-full"
+                src={user.photoURL}
+                alt=""
+              />
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu bg-base-100 rounded-box z-10 w-52 p-2 shadow-sm space-y-3"
+            >
+              <li className="font-bold">{user.displayName}</li>
+              <li>{user.email}</li>
+              <li>
+                <button
+                  type="button"
+                  onClick={handleLogOut}
+                  className="btn bg-green-600 text-white"
+                >
+                  Log Out
+                </button>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <div className="flex gap-2">
+            <Link
+              className="btn bg-gradient-to-r from-purple-800 to-purple-500 text-white"
+              to="/auth/login"
+            >
+              Login
+            </Link>
+            <Link
+              className="btn bg-gradient-to-r from-purple-800 to-purple-500 text-white"
+              to="/auth/register"
+            >
+              Register
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
