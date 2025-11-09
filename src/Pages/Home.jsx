1b788b85d14ba422d41d4e50c705399eaf330bb9
lijanahmmed <lijanahmmed@gmail.com>
2025-11-10 02:12:17 +0600

    connect with firebase

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Banner from '../Components/Banner';
-import BudgetTips from '../Components/BudgetTips';
-import FinancialMatter from '../Components/FinancialMatter';
+import React from "react";
+import Banner from "../Components/Banner";
+import BudgetTips from "../Components/BudgetTips";
+import FinancialMatter from "../Components/FinancialMatter";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <BudgetTips></BudgetTips>
-            <FinancialMatter></FinancialMatter>
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+      <BudgetTips></BudgetTips>
+      <FinancialMatter></FinancialMatter>
+    </div>
+  );
 };
 
 export default Home;
